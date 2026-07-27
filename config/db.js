1e6c0b7cb8be58edc9b57c9792d8b316c51eef6b
vasu <sadinenivenkatavasu@gmail.com>
2026-07-27 13:05:21 +0530

    commited

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const express = require('express')
 async function configureDB() {
     try {
-        await mongoose.connect(process.env.MONGO_DB);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('connected to db');
    } catch (err) {
        console.log('error connecting db', err.message)
